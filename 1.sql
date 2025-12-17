04569bd1bbdc17ef006ddc8a8a520a54aec8eab4
williemwang <1092473788@qq.com>
2025-12-17 12:03:25 +0800

    feat

--- 1.sql
+++ 1.sql
@@ -16,14 +16,36 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/`eshop` /*!40100 DEFAULT CHARACTER SET u
 
 USE `eshop`;
 
+/*Table structure for table `category` */
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cid` varchar(255) DEFAULT NULL COMMENT '类别码',
+  `cname` varchar(255) DEFAULT NULL COMMENT '类别名称',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `category` */
+
 /*Table structure for table `product` */
 
 DROP TABLE IF EXISTS `product`;
 
 CREATE TABLE `product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` varchar(255) DEFAULT NULL COMMENT '商品码',
+  `pname` varchar(255) DEFAULT NULL COMMENT '商品名称',
+  `market_price` double DEFAULT NULL COMMENT '市场价',
+  `shop_price` double DEFAULT NULL COMMENT '销售价',
+  `pimage` varchar(255) DEFAULT NULL COMMENT '图片路径',
+  `pdate` date DEFAULT NULL COMMENT '上架时间',
+  `is_hot` int(11) DEFAULT NULL COMMENT '是否热卖 0否 1是',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `product` */
 
 /*Table structure for table `user` */
 
@@ -31,8 +53,12 @@ DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL COMMENT '用户名称',
+  `password` varchar(255) NOT NULL COMMENT '密码',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
